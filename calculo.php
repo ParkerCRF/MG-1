75461276985460e740e9f2f514133b96dcbaea58
Marlon Emanuel <143754821+ParkerCRF@users.noreply.github.com>
2023-09-12 22:06:44 -0300

    Add files via upload

---
+++ calculo.php
@@ -0,0 +1,6 @@
+<?php
+
+$lado = $_POST['clcLado'];
+$calculo = 6*($lado*$lado);
+
+echo "A area do cubo é $calculo";
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cálculo do cubo</title>
+</head>
+<body>
+  <form action="calculo.php" method="post">
+    <label for="nome">Digite o lado do cubo: </label>
+    <input type="text" name="clcLado" id="lado" placeholder="Digite o lado do cubo">
+    <input type="submit" value="Enviar">
+  </form>
+</body>
+</html>
